a1c8a604e8e0aa0e9f1f3971a8176922a5a518cb
Ravandil <ravandil123321@gmail.com>
2018-05-29 14:37:39 +0200

    Added interpolation library 'Smooth.js', interpolation, min, max and time for an array

--- js/data_processing/data_processing.js
+++ js/data_processing/data_processing.js
@@ -1,15 +1,30 @@
 const contentElement = document.getElementById('krzysztof');
-const displayButton = document.getElementById('display');
-let isFirst = true;
+const processButton = document.getElementById('process');
+
+const interpolatedArray = [];
+let time = 0;
+let min = Infinity;
+let max = -Infinity;
+
 
 const proccessData = () => {
-    if (isFirst) {
-        isFirst = false;
-        wyswietl();
-    }
-    if (znalezioneDane) {
-        contentElement.textContent = znalezioneDane;
+    if (typeof znalezioneDane !== 'undefined') {
+        const interpolate = Smooth(znalezioneDane);         // cubic Hermite spline
+        const length = znalezioneDane.length;               // number of frames
+        time = length;
+
+        for (let i = 0; i < length; i += 0.1) {
+            let interpolatedValue = interpolate(i);
+            interpolatedArray.push(interpolatedValue);
+
+            if (interpolatedValue < min) {
+                min = interpolatedValue;
+            }
+            if (interpolatedValue > max) {
+                max = interpolatedValue;
+            }
+        }
     }
 }
 
-displayButton.addEventListener('click', proccessData);
+processButton.addEventListener('click', proccessData);
