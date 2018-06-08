6be1688c0014c90e70e923d77a7ae98a93b64bca
Ravandil <ravandil123321@gmail.com>
2018-06-08 17:54:17 +0200

    added data plotting for 'znalezioneDane' array

---
+++ js/user_interface/plot.js
@@ -0,0 +1,15 @@
+const plotData = () => {
+    if (typeof znalezioneDane !== 'undefined') {
+        const TESTER = document.getElementById('tester');
+        const timeArray = [...new Array(interpolatedArray.length).keys()].map(x => (x * 0.1).toFixed(1));
+         
+        Plotly.plot( TESTER, [{
+        x: [...timeArray],
+        y: [...interpolatedArray] }], {
+        margin: { t: 0 } } ); 
+    }
+};
+
+
+processButton.addEventListener('click', plotData);
+
